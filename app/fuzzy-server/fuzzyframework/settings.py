87e44df565dc9c3ff0046091b3fde341c2ac3a64
Danilo M <mdaniloraulino@gmail.com>
2019-11-06 00:14:53 -0300

    Update settings.py

--- app/fuzzy-server/fuzzyframework/settings.py
+++ app/fuzzy-server/fuzzyframework/settings.py
@@ -53,10 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = 'fuzzyframework.urls'
 
 TEMPLATES = [
@@ -126,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
