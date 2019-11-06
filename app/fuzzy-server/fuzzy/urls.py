438d03ff33cd664e8410e4a07a03ddf5e326bbde
Danilo M <mdaniloraulino@gmail.com>
2019-11-06 01:10:49 -0300

    url regra

--- app/fuzzy-server/fuzzy/urls.py
+++ app/fuzzy-server/fuzzy/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('atributo', views.AtributoViewSet)
 router.register('variavel', views.VariavelViewSet)
+router.register('regra', views.RegraViewSet)
 router.register('custom', views.VariavelCustomViewSet)
 
 urlpatterns = [
