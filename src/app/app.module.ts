f72289c3ffc1b4ce08eebfb2a8f9f4631e00c0d6
Lugano <gabrieltomazip@gmail.com>
2019-11-06 01:30:30 -0300

    cadastro regra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VariaveisComponent } from './variaveis/variaveis.component';
+import { RegrasComponent } from './regras/regras.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { VariaveisComponent } from './variaveis/variaveis.component';
   ],
   declarations: [
     AppComponent,
-    VariaveisComponent
+    VariaveisComponent,
+    RegrasComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
